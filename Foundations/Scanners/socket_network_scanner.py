b67e085d20542ad5d71a7f4d812f091d589af364
Atomic Script <46688597+AtomicScript@users.noreply.github.com>
2026-03-15 20:30:26 -0400

    Implement multi-threaded TCP port scanning
    Added multi-threading support for scanning common ports and improved banner retrieval for specific ports.

--- Foundations/Scanners/socket_network_scanner.py
+++ Foundations/Scanners/socket_network_scanner.py
@@ -1,3 +1,45 @@
 import socket
+from threading import Thread
 
-# creating a new socket
+# SOCK_STREAM -> TCP
+# SOCK_DGRAM -> UDP
+
+# NOTE: Note a stealth scanner 
+
+def full_TCP_connect_scan(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(3)
+        try:
+            result = s.connect_ex((host, port))
+            if result == 0: 
+                print(f"[+] Port {port} is open")
+                # NOTE: sending can probe a response 
+                # FIXME: specific ports will have a specific probe 
+                if port in [80, 445, 8080]:
+                    s.send(b"GET / HTTP/1.1\r\n\r\n")
+                    banner = s.recv(1024).decode(errors='ignore').strip()
+                    print(f"[+][Banner]: {banner[:50]}")
+        
+        except TimeoutError as e:
+            print(f"[-] Error: {e}")
+
+        except socket.error as e:
+            print(f"[-] Error: {e}")
+
+
+common_ports = [20, 21, 22, 23, 25, 80, 111, 443, 445, 631, 993, 995, 135, 137, 138, 139, 548, 8080]
+threads = []
+
+for port in common_ports:
+    t = Thread(target=full_TCP_connect_scan, args=("216.239.38.120", int(port)))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print("\nScan complete.")
+
+# TODO: Switch from threading to queue 
+# TODO: using scapy for Stealth (Half-Open) Scan 
+# TODO: NMAP Scanner 
